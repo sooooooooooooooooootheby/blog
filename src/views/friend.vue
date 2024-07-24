f4fabb6de2bbc2617b83e6f3582a35123d22821d
sooooooooooooooooootheby <therese.sjdyka@gmail.com>
2024-07-24 11:22:46 +0800

    添加了友链页加载提示

--- src/views/friend.vue
+++ src/views/friend.vue
@@ -2,9 +2,27 @@
 	<div class="friend">
 		<span class="title">Friend</span>
 		<span class="subTitle">These are my friends</span>
-		
+
 		<ul v-if="isFriendList">
-			<li v-for="(item, index) in friendData" :key="index">
+			<li v-if="isFriend" class="loading">
+				<div class="loader">
+					<div class="bar1"></div>
+					<div class="bar2"></div>
+					<div class="bar3"></div>
+					<div class="bar4"></div>
+					<div class="bar5"></div>
+					<div class="bar6"></div>
+					<div class="bar7"></div>
+					<div class="bar8"></div>
+					<div class="bar9"></div>
+					<div class="bar10"></div>
+					<div class="bar11"></div>
+					<div class="bar12"></div>
+				</div>
+				<p>查询中, 由于是Github的API,查询会比较慢。</p>
+				<p>如果查询失败请魔法上网或者刷新页面。</p>
+			</li>
+			<li v-for="(item, index) in friendData" :key="index" v-else>
 				<div class="background">
 					<img :src="item.avatar_url" alt="background" />
 				</div>
@@ -46,18 +64,23 @@ export default {
 		return {
 			friendList: ["sooooooooooooooooootheby", "lassksy", "himicoswilson"],
 			friendData: [],
-			isFriendList: true
+			isFriendList: true,
+			isFriend: true,
 		};
 	},
 	methods: {
+		showList() {
+			this.isFriend = !this.isFriend;
+		},
 		showErrer() {
-			this.isFriendList = !this.isFriendList
+			this.isFriendList = !this.isFriendList;
 		},
 		async getFriendInfo() {
 			const promises = this.friendList.map(async (user) => {
 				try {
 					const res = await axios.get(`https://api.github.com/users/${user}`);
 					this.friendData.push(res.data);
+					this.showList();
 				} catch (error) {
 					console.error(error);
 					this.showErrer();
